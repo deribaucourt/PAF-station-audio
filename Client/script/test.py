0fe2449ecf03cb671d60e008aaba20bfdf3fee47
François DESRICHARD <francois.desrichard@telecom-paristech.fr>
2016-06-20 09:33:02 +0200

    Client-server communication is established

--- Client/script/test.py
+++ Client/script/test.py
@@ -34,36 +34,36 @@ function sendAudio() {
     "use strict";
 
     var xhr = new GetXMLHttpRequest();
-    
-    xhr.open("POST", "/request", true);
-    xhr.setRequestHeader("Content-Type", "text");
-    
-    xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
-        alert("OK");
-    }
-    
     var sentText = encodeURIComponent("this is a test");
     
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+            alert(xhr.responseText);
+        }
+    }
+    
+    xhr.open("POST", "/request", true);
+    
+    xhr.setRequestHeader("Content-Type", "text");
     xhr.send(sentText);
     
 }
 </script>
 </head>
 <body>
-<h1 onclick='sendAudio()'>Flask AJAX Test</h1>
+<h1 onClick='sendAudio()'>Flask AJAX Test</h1>
 </body>
 </html>"""
 
 @app.route('/')
 def index():
     return html_page
-        
-        
+
+
 @app.route('/request', methods = ['POST'])
 def handle_request() :
     return "wtf"
-    
-    
+
+
 if __name__ == "__main__":
     app.run(debug = True)
